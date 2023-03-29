2734f4817dd89b7f84b6333fa44aa9b3d42e9d83
nehabirajdar <nsb9823@gmail.com>
2023-03-29 12:53:20 +0530

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            List<int> list = new List<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (int i in arr)
+            {
+                if (!list.Contains(i))
+                {
+                    list.Add(i);
+                }
+            }
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
